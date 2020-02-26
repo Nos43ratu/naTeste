d87ba7a0c777b07683a1cc0cd63dd2f67cf0c272
Adil <kairolla_adil2@mail.ru>
2020-02-26 10:18:33 +0300

    Updates

---
+++ precache-manifest.0a335240f8ace3bb81440e539ece2fc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8f05339f5d9df9ed25a5e5ef0fca75",
+    "url": "/nateste/index.html"
+  },
+  {
+    "revision": "ed7d70774c1e83c6af33",
+    "url": "/nateste/static/css/main.d43c8367.chunk.css"
+  },
+  {
+    "revision": "df060fc31d8f84187e15",
+    "url": "/nateste/static/js/2.80f0af5a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/nateste/static/js/2.80f0af5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7d70774c1e83c6af33",
+    "url": "/nateste/static/js/main.2af331a7.chunk.js"
+  },
+  {
+    "revision": "6c09c33810c1994bd272",
+    "url": "/nateste/static/js/runtime-main.a217f3c4.js"
+  },
+  {
+    "revision": "da7dd7c06dcb0e3de3a1fa9abc10512f",
+    "url": "/nateste/static/media/Snimok_ekrana_2020-01-07_v_17_00_45.da7dd7c0.png"
+  }
+]);
